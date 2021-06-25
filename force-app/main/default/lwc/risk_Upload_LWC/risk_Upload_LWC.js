d82d34418ea785889f836cd19b36c66547eb8788
Rushabh Musthyala <rmusthyala@rmusthy-ltmcjvg.internal.salesforce.com>
2021-06-25 17:55:24 +0530

    first commit

---
+++ force-app/main/default/lwc/risk_Upload_LWC/risk_Upload_LWC.js
@@ -0,0 +1,114 @@
+import csvFileRead from '@salesforce/apex/CSVFileRead.csvFileRead';
+import {ShowToastEvent} from 'lightning/platformShowToastEvent';
+import { LightningElement, api, track, wire } from 'lwc';
+import sendEmailToController from '@salesforce/apex/CSVFileRead.sendEmailToController';
+import getUserDetails from '@salesforce/apex/CSVFileRead.getUserDetails';
+import Id from '@salesforce/user/Id';
+
+
+
+export default class Risk_Upload_LWC extends LightningElement {
+    @api myRecordId;
+    @track data;
+    @track error;
+    @track docId;
+    @track subject = 'Risk Upload Results';
+    @track body = '';
+    @track toSend = 'rmusthyala@salesforce.com'; // remove before sharing
+    userId = Id;
+    @track csvAttach;
+    @track isModalOpen = false;
+    @track fileName;
+
+    // @wire(getUserDetails, {
+    //     recId: '$userId'
+    // })
+    // wiredUser({
+    //     error,
+    //     data
+    // }) {
+    //     if (data) {
+    //         this.user = data;
+    //         console.log(data.Email);
+    //         this.toSend = data.Email;
+
+    //     } else if (error) {
+    //         this.error = error;
+    //     }
+    // }
+
+    // get acceptedFormats() {
+    //     return ['.csv'];
+    // }
+
+    sendEmailAfterUpload() {
+        //console.log(this.data);
+        this.csvAttach = this.data;
+        var currentdate = new Date(); 
+        var datetime = currentdate.getDate() + "/"
+                        + (currentdate.getMonth()+1)  + "/" 
+                        + currentdate.getFullYear() + " @ "  
+                        + currentdate.getHours() + ":"  
+                        + currentdate.getMinutes();
+        this.body += 'Risks uploaded. PFA the log. <br />';
+        this.body += 'Updated at: ' + datetime; 
+        console.log('In the email function');
+        const mailContent = {body: this.body, toSend: this.toSend, subject: this.subject, csvAttach: this.csvAttach};
+        sendEmailToController(mailContent)
+            .then(() => {
+                console.log('mail sent');
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
+
+    openModal() {
+        // to open modal set isModalOpen tarck value as true
+        this.isModalOpen = true;
+    }
+    closeModal() {
+        // to close modal set isModalOpen tarck value as false
+        this.isModalOpen = false;
+    }
+    submitDetails() {
+        csvFileRead({contentDocumentId: this.docId})
+        .then(result => {
+            console.log(result);
+            this.data = result;
+            this.sendEmailAfterUpload();
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: 'Success',
+                    message: 'Risks have been created according to CSV file. An email has been sent with futher details.',
+                    variant: 'Success',
+                }),
+            );
+        })
+        .catch(error => {
+            console.log("Error: ");
+            console.log(error);
+            this.error = error;
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: 'Error',
+                    message: JSON.stringify(this.error),
+                    variant: 'error',
+                }),   
+            )
+        })
+        this.isModalOpen = false;
+    }
+
+    handleUploadFinished(event) {
+        // Get the list of uploaded files
+        //var inputName = document.getElementById("form_cols");
+        // console.log(inputName);
+        const uploadedFiles = event.detail.files[0];
+        console.log(uploadedFiles);
+        this.docId = uploadedFiles.documentId;
+        this.fileName = uploadedFiles.name;
+        console.log(this.docId);
+        this.isModalOpen = true;
+    }
+}
